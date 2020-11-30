649b7fb801e4b38842ec53c48055c1703ce57751
Angelini <angelini75@gmail.com>
2020-11-30 16:07:28 -0300

    adding comments

--- code/02_extract_values_from_covariates.R
+++ code/02_extract_values_from_covariates.R
@@ -1,5 +1,5 @@
-# join soil data with covariates
-
+##### join soil data with covariates #####################################################
+library(mapview)
 library(sf)
 library(tidyverse)
 library(raster)
@@ -8,43 +8,40 @@ library(snowfall)
 rm(list = ls())
 
 # options for raster package (define working directory)
-rasterOptions(tmpdir="/run/media/marcos/_home/marcos/tmp/",
+rasterOptions(tmpdir="/run/media/marcos/_home/marcos/tmp/", # set a temporal directory
               progress= "text",
               timer=TRUE,
               overwrite = T,
               chunksize=2e8,
               maxmemory=1e8)
 
+### Load point data ######################################################################
+# read the data created in the first script
 s <- read_csv("input_data/bs.csv") 
 
-# we convert all BS (bs1 and bs2) to 1
+# we convert all BS (bs1 and bs2) into 1
 s <- s %>% 
   mutate(z = if_else(z>=1, 1, 0))
-
+# and then we convert the table into a spatial object (with sf package)
 s <- s %>% 
   st_as_sf(coords = c("x", "y"), crs = 4326) %>% 
   as_Spatial()
 
-plot(s["z"])
+mapview(s["z"], cex = 2,lwd = 0, color = c("black", "green"))
 
-class(s)
 
-# load area of interest
-aoi <- read_sf("input_data/aoi.shp")
+# load area of interest if needed
+# aoi <- read_sf("input_data/aoi.shp")
 
+### load covariates ######################################################################
 # load rasters
 files <- list.files(path = "input_data/covs/", pattern = "tif", full.names = TRUE)
-r <- stack()
+r <- stack(files)
 
-for (i in seq_along(files)) {
-  x <- raster(files[i])
-  x <- crop(x, y = aoi)
-  x <- mask(x, aoi)
-  r <- stack(x, r)
-  print(i)
-}
-r
-# extract in parallel 
+plot(r)
+
+# Now, we have to extract the values of the rasters at each point location. This process 
+# can be very heavy computationally, so we do it in parallel processing 
 # from https://gis.stackexchange.com/questions/253618/r-multicore-approach-to-extract-raster-values-using-spatial-points
 
 # Extract values to a data frame - multicore approach
@@ -61,15 +58,15 @@ sfLibrary(sp)
 # Run parallelized 'extract' function and stop cluster
 e.df <- sfSapply(r.list, extract, y=s, method='simple')
 sfStop()
-class(e.df)
+e.df
 
-
+colSums(is.na(e.df))[colSums(is.na(e.df))>1]
 # Fix resulting data frame
 s <- st_as_sf(s)
 DF <- st_bind_cols(s, e.df) 
+DF <- cbind(DF,st_coordinates(DF))
 DF <- DF %>% st_drop_geometry()
-s <- read_csv("input_data/bs.csv")
-s <- dplyr::left_join(s,DF) %>% na.omit()
+s <- DF %>% na.omit()
 
 write_csv(s, "input_data/soil_&_covariates.csv")
 
