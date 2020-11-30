1cbc15bb91b5e29f0815a0bc220bb5698cb1e38c
Angelini <angelini75@gmail.com>
2020-11-30 16:06:41 -0300

    small edits

--- code/01_preprocess_data.R
+++ code/01_preprocess_data.R
@@ -13,7 +13,7 @@ paste("number of soil profiles" ,length(unique(dat$idp)))
 ## Explore the data
 # histograms 
 dat %>% 
-  select(top, bot, cec, oc, w_value, w_chroma, bsat) %>% 
+  dplyr::select(top, bot, cec, oc, w_value, w_chroma, bsat) %>% 
   pivot_longer(top:bsat) %>% 
   na.omit() %>% 
   ggplot(aes(x = value, color = name)) + 
@@ -22,7 +22,7 @@ dat %>%
 
 # soil profile distribution          
 dat %>% 
-  select(x, y) %>% 
+  dplyr::select(x, y) %>% 
   unique() %>% 
   st_as_sf(coords = c("x","y"), crs = 4326) %>% 
   mapview(cex = 2, lwd = 0)
@@ -36,24 +36,29 @@ dat %>%
          bs_bsat = if_else(condition = bsat >= 50, true = 1, false = 0),
          bs_cec = if_else(condition = cec >= 25, true = 1, false = 0)))
 
+# show points in a map
 x %>% 
-  select(x, y, bs_oc) %>% 
+  dplyr::select(x, y, bs_oc) %>% 
   unique() %>% 
   st_as_sf(coords = c("x","y"), crs = 4326) %>% 
   mapview(cex = 3, lwd = 0)
 
-(x <- x %>% select(idh:bot, bs_oc:bs_cec))
+# select useful columns
+(x <- x %>% dplyr::select(idh:bot, bs_oc:bs_cec))
 
 
 # Classify the profiles as BS or no-BS
 # 1st. We create two columns bs1 and bs2 to classify horizons
 x <- x %>% 
-  mutate(bs1 = ifelse(bs_oc == 1 & bs_w_chroma == 1 & bs_w_value == 1 & bs_cec == 1 & bs_bsat == 1,
-                     yes = 1, 
-                     no = 0),
+  mutate(bs1 = ifelse(bs_oc == 1 & bs_w_chroma == 1 & bs_w_value == 1 & 
+                        bs_cec == 1 & bs_bsat == 1,
+                      yes = 1, 
+                      no = 0),
          bs2 = ifelse(bs_oc == 1 & bs_w_chroma == 1 & bs_w_value == 1,
                       yes = 1,
                       no = 0)) 
+# then, a column with three values are created: 0 for non-BS, 1 for 1st cat. BS, and
+# 2 for 2nd cat. BS
 x <- x %>% 
   group_by(idp) %>% 
   summarise(x = first(x),
@@ -70,12 +75,14 @@ x <- x %>%
             z = ifelse(bs1 == 1 & bottom == 1, 1, 
                        ifelse(bs2 == 1 & bottom == 1, 2, 0)))
 
+# Finally we visualise the data in a map and save it
 x %>% 
-  select(x, y, z) %>% 
+  dplyr::select(x, y, z) %>% 
   na.omit() %>% 
   st_as_sf(coords = c("x","y"), crs = 4326) %>% 
   mapview(cex = 3, lwd = 0)
 
-table(x$z)
+# how many samples the classes have?
+table(x$z) # note that 1 and 2 are both BS
 
 write_csv(x, "input_data/bs.csv")
